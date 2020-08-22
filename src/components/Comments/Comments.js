2ff7e15cece16572f23a8f2e2b8ed101132f0b07
adelazalewski <paliuadela@yahoo.com>
2020-08-22 18:55:13 -0400

    finished mvp but i am getting a module not found error

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Comment from './Comment';
 import './Comments.css';
+import PostHeader from '../Posts/PostHeader';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
+console.log(comments);
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment => <Comment comment={comments} />)}
     </div>
   );
 };
 
 export default Comments;
+// // app - searchbar
+// //     -posts - post - PostHeader
+//                      - comments - comment
+                                
+//                      - likesection
